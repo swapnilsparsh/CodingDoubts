6ff920579ecda98b262046a2583307e7bb9d9458
swapnilsparsh <arezona.lucky55@gmail.com>
2023-05-02 04:51:07 +0530

    Footer Repo link fix

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="text-center text-primary-white">
 	<p class="mt-8">
 		Made by <a
-			class=" text-primary-CetaceanBlue underline hover:text-primary-SpanishBlue"
+			class="underline text-primary-CetaceanBlue hover:text-primary-SpanishBlue"
 			href="https://swapnilsparsh.github.io/"
 			target="_blank">Swapnil Srivastava</a
 		>
@@ -9,7 +9,7 @@
 	<p class="mt-1 text-sm text-primary-OffGray">⚠️ Warning: Answers may not be accurate.</p>
 	<p class="text-sm text-primary-OffGray">
 		<a
-			href="https://github.com/swapnilsparsh/CodeMentor"
+			href="https://github.com/swapnilsparsh/CodingDoubts"
 			target="_blank"
 			class="underline hover:text-gray-400">Source</a
 		>
